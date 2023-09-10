f522c63016bf3f97209fb015dbcd97255159c09f
Zaid <zaidkamboo100@gmail.com>
2023-09-10 11:12:36 +0530

    Completed with the Desktop Clone

--- src/Components/Scrollbar.jsx
+++ src/Components/Scrollbar.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import "./CSS/Scrollbar.css"
+import React from "react";
+import "./CSS/Scrollbar.css";
 const Scrollbar = ({ items }) => {
-    return (
-        <div style={{ height: '300px', overflowY: 'scroll', overflowX: "hidden" }} className='sb'>
-            <ul className='topTeams'>
-                {items.map((item, index) => (<>
-                    <li key={index} className='topTeam'>
-                        <div className="top">
-                            <div className="lim">
-                                <img src={item.logo} alt="" className="log" />
-                            </div>
-                            <div className="Name mx-2">
-                                {item.name}
-                            </div>
-                            <div className="but" >
-                                <button className="btn">{item.type}</button>
-                            </div>
-                        </div>
-                    </li>
-                </>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div
+      style={{ height: "300px", overflowY: "scroll", overflowX: "hidden" }}
+      className="sb"
+    >
+      <ul className="topTeams">
+        {items.map((item, index) => (
+          <div key={item.name} className="lis">
+            <li className="topTeam">
+              <div className="top">
+                <div className="lim">
+                  <img src={item.logo} alt="" className="log" />
+                </div>
+                <div className="Name mx-2">{item.name}</div>
+                <div className="but">
+                  <button className="bton">{item.type}</button>
+                </div>
+              </div>
+            </li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Scrollbar;
